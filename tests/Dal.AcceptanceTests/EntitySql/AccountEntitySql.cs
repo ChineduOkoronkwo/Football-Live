5ca0ae3770d6d4f11b0c13df05b84433df1728d1
unknown <okoronkwoac@yahoo.com>
2024-02-04 00:24:39 -0500

    extended test

--- tests/Dal.AcceptanceTests/EntitySql/AccountEntitySql.cs
+++ tests/Dal.AcceptanceTests/EntitySql/AccountEntitySql.cs
@@ -8,7 +8,7 @@ namespace Dal.AcceptanceTests.EntitySql
             => "SELECT Id, CurrencyId, AccountTypeId, CustomerId, Name, Description, Balance FROM Account WHERE Id = @Id;";
 
         public string ListSqlCommand => "SELECT Id, CurrencyId, AccountTypeId, CustomerId, Name, Description, Balance"
-            + " FROM Account ORDER BY CurrencyId LIMIT @PageSize OFFSET @PageOffset;";
+            + " FROM Account ORDER BY CurrencyId, Name, AccountTypeId LIMIT @PageSize OFFSET @PageOffset;";
 
         public string CreateSqlCommand => "INSERT INTO Account(Id, CurrencyId, AccountTypeId, CustomerId, Name, Description, Balance)"
             + " VALUES(@Id, @CurrencyId, @AccountTypeId, @CustomerId, @Name, @Description, @Balance);";
